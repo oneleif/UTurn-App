765e8ecd1f7013e564ea2a9a44377636672106cd
Vancer <supremevance@gmail.com>
2020-07-10 10:09:30 -0500

    Initial Commit

---
+++ UTurn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UTurn
+//
+//  Created by Vance Mchargue on 7/10/20.
+//  Copyright © 2020 Vance Mchargue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
